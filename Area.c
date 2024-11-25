d8c84c21e18aaa6c4436d9e4b54214ae58b9628a
edmond7112 <edmondmatara44@gmail.com>
2024-11-25 11:46:44 +0300

    Add files via upload

---
+++ Area.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+int main() {
+     
+    int l,w,Area;
+    Area=l*w;
+    printf("Enter the first number");
+    scanf("%d",&l);
+    printf("Enter the second number");
+    scanf("%d",&w);
+    Area=l*w;
+    printf("The Area of A Rectangle is %d",Area);
+    
+    return 0;
+}
