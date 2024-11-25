fda09d6d5fd64b0ac99bdb3065f8faa1b9bcdf91
edmond7112 <edmondmatara44@gmail.com>
2024-11-25 12:05:00 +0300

    Add files via upload

---
+++ Volume of cylinder.c
@@ -0,0 +1,14 @@
+//Program to Find Volume of a Cylinder
+#include <stdio.h>
+int main() {
+    double pi,r,h,v;
+printf("input pi value.\n");
+scanf("%lf",&pi);
+printf("enter r value.\n");
+scanf("%lf",&r);
+printf("enter h value.\n");
+scanf("%lf",&h);
+v=pi*r*r*h;
+printf("volume:%.4lf\n",v);
+    return 0;
+}
